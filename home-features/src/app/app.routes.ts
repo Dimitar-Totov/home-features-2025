6d65188623b933de3365afdb518f2b0a792d0cea
Dimitar-Totov <dimitar.totov@abv.bg>
2025-07-15 18:01:17 +0300

    Add about us component

--- home-features/src/app/app.routes.ts
+++ home-features/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ProductBoard } from './features/products/product-board/product-board';
 import { Home } from './shared/components/home/home';
+import { AboutUs } from './shared/components/about-us/about-us';
 
 export const routes: Routes = [
     { path: '', component: Home },
-    { path: 'catalog', component: ProductBoard }
+    { path: 'catalog', component: ProductBoard },
+    { path: 'about', component: AboutUs }
 ];
--- home-features/src/app/app.ts
+++ home-features/src/app/app.ts
@@ -4,10 +4,11 @@ import { Header } from './shared/components/header/header';
 import { Footer } from './shared/components/footer/footer';
 import { ProductBoard } from './features/products/product-board/product-board';
 import { Home } from './shared/components/home/home';
+import { AboutUs } from './shared/components/about-us/about-us';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home, Header, Footer, ProductBoard],
+  imports: [RouterOutlet, Home, Header, Footer, ProductBoard, AboutUs],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
