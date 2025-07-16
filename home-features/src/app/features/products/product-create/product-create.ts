4189b53e86cb5d0db5acdd7c3fb0366faa480852
Dimitar-Totov <dimitar.totov@abv.bg>
2025-07-16 19:51:57 +0300

    Add product create component

---
+++ home-features/src/app/features/products/product-create/product-create.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-product-create',
+  imports: [RouterLink],
+  templateUrl: './product-create.html',
+  styleUrl: './product-create.css'
+})
+export class ProductCreate {
+
+}
