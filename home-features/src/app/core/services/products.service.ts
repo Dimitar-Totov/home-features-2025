c6660c562326b924757e710311f54954632d174e
Dimitar-Totov <dimitar.totov@abv.bg>
2025-08-14 18:10:08 +0300

    Add product service

---
+++ home-features/src/app/core/services/products.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Product } from "../../models";
+
+
+@Injectable({ providedIn: 'root' })
+
+export class ProductService {
+    private apiUrl = 'http://localhost:3000/api/products';
+
+    constructor(private httpClient: HttpClient) {
+
+    }
+
+    getProducts(): Observable<Product[]> {
+        return this.httpClient.get<Product[]>(this.apiUrl);
+    }
+
+    createProduct(productName: string,
+        productCategory: string,
+        productPrice: string,
+        productColor: string,
+        productDimensions: string,
+        productDescription: string) {
+        const body = JSON.stringify({ productName, productCategory, productPrice, productColor, productDimensions, productDescription });
+        return this.httpClient.post<Product>(this.apiUrl, body, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+    }
+}
