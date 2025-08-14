5d266bf795505a77c141ffaff168dd874302011b
Dimitar-Totov <dimitar.totov@abv.bg>
2025-08-14 18:08:05 +0300

    Fix paths

--- home-features/src/app/app.ts
+++ home-features/src/app/app.ts
@@ -3,10 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/components/header/header';
 import { Footer } from './shared/components/footer/footer';
 import { ProductBoard } from './features/products/product-board/product-board';
-import { Home } from './shared/components/home/home';
-import { AboutUs } from './shared/components/about-us/about-us';
-import { Login } from './shared/components/login/login';
-import { Register } from './shared/components/register/register';
+import { Home } from './features/home/home';
+import { AboutUs } from './features/about-us/about-us';
+import { Login } from './features/auth/login/login';
+import { Register } from './features/auth/register/register';
 import { ProductCreate } from './features/products/product-create/product-create';
 import { NotFound } from './shared/components/not-found/not-found';
 
