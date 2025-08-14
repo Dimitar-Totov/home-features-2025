f1f43208b510c4f920bc7c316d8a6ddc395054f5
Dimitar-Totov <dimitar.totov@abv.bg>
2025-08-14 18:08:39 +0300

    Add error interceptor

---
+++ home-features/src/app/http-error-interceptor.ts
@@ -0,0 +1,19 @@
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
+
+export const httpErrorInterceptor: HttpInterceptorFn = (req, next) => {
+  const router = inject(Router);
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+
+      if (error.status === 401) {
+        router.navigate(['/login']);
+      }
+      alert(`Error ${error.status}`);
+
+      return throwError(() => error)
+    })
+  );
+};
