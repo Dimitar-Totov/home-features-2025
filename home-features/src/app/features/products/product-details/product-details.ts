066e4392d1dedd95e9da87cc7de8f45d9993534c
tananska <ivatananska@abv.bg>
2025-08-17 19:37:17 +0300

    Create product details component

---
+++ home-features/src/app/features/products/product-details/product-details.ts
@@ -0,0 +1,28 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { AuthService, ProductService } from '../../../core/services';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../../../models';
+
+@Component({
+  selector: 'app-product-details',
+  imports: [],
+  templateUrl: './product-details.html',
+  styleUrl: './product-details.css'
+})
+export class ProductDetails implements OnInit {
+  product: Product = {name:'',category:'',price:'',description:'',dimensions:'',imageUrl:'',color:'',_id: 0};
+  productId!: string;
+
+  private authService = inject(AuthService);
+
+  constructor(private productService: ProductService, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.productId = this.route.snapshot.paramMap.get('productId')!;
+    this.productService.getProduct(this.productId).subscribe({
+      next: (data) => this.product = data,
+      error: (err) => console.log(err),
+    })
+
+  }
+}
