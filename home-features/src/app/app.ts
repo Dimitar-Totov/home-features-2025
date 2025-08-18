3fe1077736d58829c565576af4f3abbb7a5e0c9b
tananska <ivatananska@abv.bg>
2025-08-18 15:25:28 +0300

    Remove unused imports in app

--- home-features/src/app/app.ts
+++ home-features/src/app/app.ts
@@ -2,18 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/components/header/header';
 import { Footer } from './shared/components/footer/footer';
-import { ProductBoard } from './features/products/product-board/product-board';
-import { Home } from './features/home/home';
-import { AboutUs } from './features/about-us/about-us';
-import { Login } from './features/auth/login/login';
-import { Register } from './features/auth/register/register';
-import { ProductCreate } from './features/products/product-create/product-create';
-import { NotFound } from './shared/components/not-found/not-found';
 import { ErrorNotification } from './shared/components/error-notification/error-notification';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home, Header, Footer, ProductBoard, AboutUs, Login, Register, ProductCreate, NotFound, ErrorNotification],
+  imports: [RouterOutlet, Header, Footer, ErrorNotification],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
