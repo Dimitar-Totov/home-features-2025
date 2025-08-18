d682fa273817c1f928623305292747f2525a2a04
tananska <ivatananska@abv.bg>
2025-08-18 15:24:10 +0300

    Add delete method

--- home-features/src/app/features/products/product-details/product-details.ts
+++ home-features/src/app/features/products/product-details/product-details.ts
@@ -1,17 +1,18 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { AuthService, ProductService } from '../../../core/services';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { Product } from '../../../models';
 
 @Component({
   selector: 'app-product-details',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './product-details.html',
   styleUrl: './product-details.css'
 })
 export class ProductDetails implements OnInit {
-  product: Product = {name:'',category:'',price:'',description:'',dimensions:'',imageUrl:'',color:'',_id: 0};
+  product: Product = { name: '', category: '', price: '', description: '', dimensions: '', imageUrl: '', color: '', _id: 0 };
   productId!: string;
+  private router = inject(Router);
 
   private authService = inject(AuthService);
 
@@ -23,6 +24,15 @@ export class ProductDetails implements OnInit {
       next: (data) => this.product = data,
       error: (err) => console.log(err),
     })
+  }
 
+  deleteButton(): void {
+    const confirmed = confirm('Are you sure you want to delete this product ?');
+    if (confirmed) {
+      this.productService.deleteProduct(this.productId).subscribe({
+        next: () => this.router.navigate(['/catalog']),
+        error: (err) => console.log(err),
+      })
+    }
   }
 }
