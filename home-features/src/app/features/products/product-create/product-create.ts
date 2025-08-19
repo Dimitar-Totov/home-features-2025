a07b15f854b07476063b4da5e3353504fe9e7f21
tananska <ivatananska@abv.bg>
2025-08-19 16:07:55 +0300

    Add validators for create page fields

--- home-features/src/app/features/products/product-create/product-create.ts
+++ home-features/src/app/features/products/product-create/product-create.ts
@@ -13,39 +13,108 @@ import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } 
 export class ProductCreate {
   productFormGroup: FormGroup;
 
-  
+
   constructor() {
     this.productFormGroup = new FormGroup({
-      name: new FormControl('', [Validators.minLength(5)]),
-      category: new FormControl(''),
-      price: new FormControl(''),
-      color: new FormControl(''),
-      dimensions: new FormControl(''),
-      description: new FormControl(''),
+      name: new FormControl('', [Validators.pattern(/^[A-Za-z]{5,}$/), Validators.required]),
+      category: new FormControl('', [Validators.pattern(/^[A-Za-z]{3,}$/)]),
+      price: new FormControl('', [Validators.pattern(/^1\d{1,}$/)]),
+      color: new FormControl('', [Validators.pattern(/^[A-Za-z]{3,}$/)]),
+      dimensions: new FormControl('', [Validators.pattern(/^[A-Za-z0-9]{1,15}$/)]),
+      description: new FormControl('', [Validators.pattern(/^[A-Za-z0-9.!]{10,}$/)]),
     })
   }
-  
+
   private authService = inject(AuthService);
   private productService = inject(ProductService);
   private router = inject(Router);
-  
+
   readonly isLoggedIn = this.authService.isLoggedIn();
   readonly currentUser = this.authService.currentUser();
+
+  nameErrorMessage: string = '';
+  categoryErrorMessage: string = '';
+  priceErrorMessage: string = '';
+  colorErrorMessage: string = '';
+  dimensionsErrorMessage: string = '';
+  descriptionErrorMessage: string = '';
+
 
   onCancel(): void {
     this.router.navigate(['/']);
   }
 
   onSubmit(): void {
-    const productData = this.productFormGroup.value;
-    const ownerId = this.currentUser?.id;
-    this.productService.createProduct(productData,ownerId!).subscribe({
-      next: () => {
-        this.router.navigate(['/catalog'])
-      },
-      error: (err) => {
-        console.log(err);
+    if (this.productFormGroup.valid) {
+      const productData = this.productFormGroup.value;
+      productData.likes = [];
+      const ownerId = this.currentUser?.id;
+      this.productService.createProduct(productData, ownerId!).subscribe({
+        next: () => {
+          this.router.navigate(['/catalog'])
+        },
+        error: (err) => {
+          console.log(err);
+        }
+      })
+    } else {
+      const nameError = this.productFormGroup.get('name');
+      const categoryError = this.productFormGroup.get('category');
+      const priceError = this.productFormGroup.get('price');
+      const colorError = this.productFormGroup.get('color');
+      const dimensionsError = this.productFormGroup.get('dimensions');
+      const descriptionError = this.productFormGroup.get('description');
+
+      if (nameError?.errors?.['pattern']) {
+        this.nameErrorMessage = 'The name of the product must contain only letters and be at least 5 characters long!';
       }
-    })
+      if (nameError?.hasError('required')) {
+        this.nameErrorMessage = 'Name field is empty!';
+      }
+
+      if (categoryError?.errors?.['pattern']) {
+        this.categoryErrorMessage = 'The category of the product must contain only letters and be at least 3 characters long!';
+      }
+      if (categoryError?.hasError('required')) {
+        this.categoryErrorMessage = 'Category field is empty!';
+      }
+
+      if (priceError?.errors?.['pattern']) {
+        this.priceErrorMessage = 'The price of the product must contain only numbers and have at least 2 digits!';
+      }
+      if (priceError?.hasError('required')) {
+        this.priceErrorMessage = 'Price field is empty!';
+      }
+
+      if (colorError?.errors?.['pattern']) {
+        this.colorErrorMessage = 'The color of the product must contain only letters and be at least 3 characters long!';
+      }
+      if (colorError?.hasError('required')) {
+        this.colorErrorMessage = 'Color field is empty!';
+      }
+
+      if (dimensionsError?.errors?.['pattern']) {
+        this.dimensionsErrorMessage = 'The dimensions of the product can contain only letters and numbers, and must be at most 15 characters long!';
+      }
+      if (dimensionsError?.hasError('required')) {
+        this.dimensionsErrorMessage = 'Dimensions field is empty!';
+      }
+
+      if (descriptionError?.errors?.['pattern']) {
+        this.descriptionErrorMessage = 'The description of the product can contain letters, numbers, . and !, and must be at most 15 characters long!';
+      }
+      if (descriptionError?.hasError('required')) {
+        this.descriptionErrorMessage = 'Description field is empty!';
+      }
+
+      setTimeout(() => {
+        this.nameErrorMessage = '';
+        this.categoryErrorMessage = '';
+        this.priceErrorMessage = '';
+        this.colorErrorMessage = '';
+        this.dimensionsErrorMessage = '';
+        this.descriptionErrorMessage = '';
+      }, 5000)
+    }
   }
 }
